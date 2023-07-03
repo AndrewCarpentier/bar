424cefe9cec21d08b9015ee8de086b121cedc1e8
olivier-webdev <olivier.webdev@gmail.com>
2023-07-03 22:15:36 +0200

    add docker file

--- API_BAR/db/configDB.js
+++ API_BAR/db/configDB.js
@@ -1,7 +1,8 @@
 module.exports = {
   mongoDb: {
-    // uri: "mongodb+srv://oli-dwwm:12345@cluster0.hqoles9.mongodb.net/bar?retryWrites=true&w=majority",
-    uri : "mongodb+srv://andrewC:123@jwt.ktqd7hj.mongodb.net/bar?retryWrites=true&w=majority",
+    uri: "mongodb+srv://oli-dwwm:12345@cluster0.hqoles9.mongodb.net/bar?retryWrites=true&w=majority",
+    // uri: "mongodb+srv://andrewC:123@jwt.ktqd7hj.mongodb.net/bar?retryWrites=true&w=majority",
+    // uri: "mongodb+srv://vince:12345@cluster0.ukygbxh.mongodb.net/bar?retryWrites=true&w=majority",
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
